dcfe813b98f110ba952945621f50fd8dd90239dd
祥 赵 <zxzsm@163.com>
2020-03-02 15:44:22 +0800

    123

---
+++ tbk/src/main/java/com/tbk/TbkApplication.java
@@ -0,0 +1,13 @@
+package com.tbk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TbkApplication.class, args);
+	}
+
+}
